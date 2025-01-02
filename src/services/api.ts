d7481c7557cc271a8f0b55b145fac97b930bb0be
rodgc <rod.gc@hotmail.com>
2025-01-02 02:54:00 -0600

    Add API service for fetching popular and search movies

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+const API_KEY = '2df60a7696d5e1bc08f620d0adaf4577';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query: string) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query,
+    )}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
