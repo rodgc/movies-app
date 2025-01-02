499c45966b83e53b01314dc8da99a394471a94be
rodgc <rod.gc@hotmail.com>
2025-01-02 02:54:54 -0600

    Add Favorites page with empty state and styling

---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import './Favorites.css';
+export const Favorites = () => {
+  return (
+    <div className='favorites-empty'>
+      <h2>No favorites Movies yet</h2>
+      <p>
+        Start adding movies to your favorites by clicking the heart icon on the
+        movie card
+      </p>
+    </div>
+  );
+};
